95377a2558d9931d02a07abf367f2a3d1fe98075
FutureApp <michael-czaja-arbeit@hotmail.de>
2018-12-29 18:25:10 +0100

    Create buntu_soruce-file.sh

---
+++ buntu_soruce-file.sh
@@ -0,0 +1,16 @@
+source <(kubectl completion bash)
+alias watchk="watch kubectl get pods --all-namespaces"
+alias bye="sudo shutdown now"
+alias reb="sudo reboot now"
+
+alias mgitb="git branch"
+alias mgits="git branch; git status"
+alias mgitc="git commit -m "
+alias dockeri="docker images"
+alias dockerrmiton="docker "
+alias wk="watch kubectl get pods --all-namespaces"
+
+eval $(minikube docker-env)
+alias mini="minikube "
+alias cd..="cd .."
+alias kube="kubectl "
