b5efa7e3e0a0ed1802cb7e076f47fd68ef6dc391
FutureApp <michael-czaja-arbeit@hotmail.de>
2019-01-24 13:05:16 +0100

    Update buntu_soruce-file.sh

--- bunutu/buntu_soruce-file.sh
+++ bunutu/buntu_soruce-file.sh
@@ -1,5 +1,3 @@
-#!/bin/bash 
-echo" 
 source <(kubectl completion bash)
 alias watchk="watch kubectl get pods --all-namespaces"
 alias bye="sudo shutdown now"
@@ -15,4 +13,4 @@ alias wk="watch kubectl get pods --all-namespaces"
 alias mini="minikube "
 alias cd..="cd .."
 alias kube="kubectl "
-eval $(minikube docker-env)" >> ~/.bashrc
+eval $(minikube docker-env)
